6379490bca8f2eaf0d772ab067dfd54b9ac26ecd
Vladislav Zavjalov <slazav@altlinux.org>
2012-03-31 19:19:55 +0400

    index scripts

--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
 IMG_NAME=hr.img
 
-RNG_FAN=150000x150000+12350000+4250000
-RNG_PAM=450000x350000+13000000+4050000
-RNG_ZAB=300000x200000+19300000+6020000
-
 out:
 	. ./settings.sh; make_out.sh
 in:
@@ -17,9 +13,16 @@ img:
 	sed -e "/${IMG_NAME}/s/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}/$(date +%F)/"\
 	  -i /home/sla/CH/data/maps/index.m4i
 
-index:
-	. ./settings.sh; map_update_index.sh 600000x400000+7085000+5970000
+index: index_fan index_pam index_zab index_saj
 
+index_fan:
+	. bin/map_update_index.sh 150000x130000+12360000+4270000 fan
+index_pam:
+	. bin/map_update_index.sh 250000x350000+13180000+4050000 pam
+index_zab:
+	. bin/map_update_index.sh 260000x200000+19310000+6020000 zab
+index_saj:
+	. bin/map_update_index.sh 390000x430000+17260000+5610000 saj
 
 update_passes:
 	map_wp_upd_nom vmap/*.vmap
--- bin/map_update_index.sh
+++ bin/map_update_index.sh
@@ -1,64 +1,75 @@
 #!/bin/sh -eu
 
-OUT_DIR=${OUT_DIR:-out}
-GEOM=$1
+OUT_DIR=${OUT_DIR:-OUT}
+. mapsoft_crd.sh
 
-if [ -z "$GEOM" ]; then
-  echo "use: $0 <geom>"
+if [ "$#" != 2 ]; then
+  echo "use: $0 <geom> <base>"
   exit 1
 fi
-olddir="$(pwd)"
+
+geom=$1
+base=$2
+
+jpeg_scale=0.2
+jpg=all_$base.jpg
+htm=all_$base.htm
+xml=all_$base.xml
+img=all_$base.img
+txt=all_$base.txt
+
 cd "$OUT_DIR"
 
-for i in *.png; do
-
-  [ -n "${i%%*_*}" ] || continue
-
-  name=${i%.png}
-
-
-  [ "$i" -ot "$name.jpg" ] || ../bin/image_resize.sh 1 5 "$i" "$name.jpg"
-
-  imgref=
-  [ ! -s "$name.img" ] ||
-    imgref="<a href="$name.img">[IMG]</a>"
-
-  [ -f "$name.htm" ] || \
-cat > "$name.htm" <<EOF
-<html>
-<head>
-  <title>$name</title>
-  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=koi8-r">
-</head>
-<body>
-<div align=center>
-  <h2>$name</h2>
-  <p><a href="$name.png"><img src="$name.jpg" align=center></a>
-<p>
- <a href="$name.png">[PNG]</a>
- <a href="$name.map">[MAP]</a>
- <a href="$name.mp.zip">[MP]</a>
- $imgref
- <a href="$name.ocd.zip">[OCD9 - incomplete!]</a>
-</div>
-</body></html>
-EOF
-
-
+# calculate map list
+maps=''
+imgs=''
+upd=''
+for n in $(geom2nom "$geom" 100000); do
+  [ "$n.png" -ot "$htm" ] || upd=1;
+  [ ! -f "$n.map" ] || maps="$maps $n.map"
+  [ ! -f "$n.img" ] || imgs="$imgs $n.img"
 done
-mapsoft_convert *.map  *.plt --rescale_maps=0.2 -o maps.xml
-sed -i 's/.png/.jpg/g' maps.xml
+[ "$txt" -ot "$htm" ] || upd=1;
 
-sed -i -e '/<\/\?maps>/d' maps.xml
+if [ -z "$upd" ]; then echo "no need to update $base index"; exit 0; fi
 
-mapsoft_convert maps.xml -g "$GEOM" -o index.jpg\
-  --draw_borders=1 --dpi=3 --scale=2e-5 --htm=index.htm
+# make xml with all small jpeg maps; make index imageand htm
+mapsoft_convert $maps *.plt --rescale_maps=$jpeg_scale -o "$xml"
+sed -i -e 's/.png/.jpg/g' "$xml"
+mapsoft_convert "$xml" --geom "$geom" -o "$jpg"\
+  --draw_borders --dpi=4 --rscale=100000 --htm="tmp.htm"
 
-sed -i -e '
+cat > "$htm" <<-EOF
+	<html>
+        <head>
+	  <title>$name</title>
+	  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=koi8-r">
+	</head>
+        <body>
+	EOF
+[ ! -f "$txt" ] || cat "$txt" >> $htm
+sed -e '
   /^<area/{
-    s/"\([^\."]*\)\.jpg" alt="" title=""/"\1.htm" alt="\1" title="\1"/;
-    s/"\([^\."]*\)\.jpg"/"\1.htm"/;
-    s/ href="..\/skl_map\/[^"]*"//;
-  }' index.htm
+    s/"\([^\."]*\)\.jpg" alt="" title=""/"\1.htm" alt="\1" title="\1"/
+    s/"\([^\."]*\)\.jpg"/"\1.htm"/
+    s/ href="..\/skl_map\/[^"]*"//
+    /<\/html>/d
+    /<html>/d
+  }' tmp.htm >> "$htm"
 
-cd "$olddir"
+d="$(date +"%F %T")"
+
+cat >> "$htm" <<-EOF
+	<p><a href="$img">Склейка векторных карт в формате IMG (typ-файл включен)</a></p>
+	<p><a href="$xml">Привезка всех листов в формате mapsoft</a></p>
+        <div align=right><i>/$d/</i></div>
+	</body></html>
+	EOF
+
+# make more useful xml
+mapsoft_convert $maps -o "$xml"
+
+# make img file
+gmt -j -v -m "slazav-$base" -o $img $imgs /usr/share/mapsoft/slazav.typ
+
+cd -
